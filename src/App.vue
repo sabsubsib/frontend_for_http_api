2b7d4a5930bd543498384cece638eaf783b86075
fractalman-7 <fractalman.id7@gmail.com>
2021-08-22 20:34:10 +0300

    New look and minor improvements

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,35 @@
 <template>
-  <p v-if="message !== null" id="alert">{{ message }}</p>
-  <form @submit="onSubmit">
-    <select v-model="selectedCode" required>
-      <option v-for="code in currencyCodes" :key="code" :value="code">
-        {{ code }}
-      </option>
-    </select>
-    <div class="input_date">
-      <label for="date1">input date1</label>
-      <input type="date" name="date1" id="date1" required v-model="date1" />
-    </div>
-    <div class="input_date">
-      <label for="date2">input date2</label>
-      <input type="date" name="date2" id="date2" required v-model="date2" />
-    </div>
-    <button type="submit" name="btn">Submit</button>
-  </form>
-  <p v-if="currencyRatesDiff !== null">{{ currencyRateDiff }}</p>
+  <main class="box">
+    <p v-if="errorMessage !== null" id="alert">{{ errorMessage }}</p>
+    <h2>Difference in {{ selectedCode }} rate:</h2>
+    <p id="diff" v-if="currencyRateDiff !== null">
+      {{ currencyRateDiff }} &#8381;
+    </p>
+    <p id="diff" v-else-if="errorMessage !== null">Error</p>
+    <p id="diff" v-else>Loading...</p>
+    <form @submit="onSubmit">
+      <div class="input-date">
+        <label for="date1">First date</label>
+        <input type="date" name="date1" id="date1" v-model="date1" required />
+      </div>
+      <div class="input-date">
+        <label for="date2">Second date</label>
+        <input type="date" name="date2" id="date2" v-model="date2" required />
+      </div>
+      <div>
+        <button type="submit" name="" style="float: left">Submit</button>
+      </div>
+      <select
+        v-model="selectedCode"
+        required
+      >
+        <option v-for="code in currencyCodes" :key="code" :value="code">
+          {{ code }}
+        </option>
+      </select>
+    </form>
+  </main>
+  <footer></footer>
 </template>
 
 <script>
@@ -31,32 +44,17 @@ export default {
       date1: null,
       date2: null,
       currencyRateDiff: null,
-      message: null,
+      errorMessage: null,
     };
   },
   methods: {
     onSubmit: function (e) {
-      const path = "http://127.0.0.1:8000/currency_rates_diff";
       this.currencyRateDiff = null;
-      this.message = null;
+      this.errorMessage = null;
       if (this.date1 > this.date2) {
-        this.message = "Invalid input";
+        this.errorMessage = "First date should be more second";
       } else {
-        axios({
-          method: "GET",
-          url: path,
-          params: {
-            code: this.selectedCode,
-            date1: this.date1,
-            date2: this.date2,
-          },
-        })
-          .then((res) => {
-            this.currencyRateDiff = res.data;
-          })
-          .catch((error) => {
-            this.message = error.response.data.detail;
-          });
+        this.getCurrencyRateDiff(this.selectedCode, this.date1, this.date2);
       }
       e.preventDefault();
     },
@@ -68,27 +66,146 @@ export default {
           this.currencyCodes = res.data;
         })
         .catch((error) => {
-          this.message = error.response.data.detail;
+          this.errorMessage = error.response.data.detail;
         });
     },
+    getCurrencyRateDiff: function (code, date1, date2) {
+      const path = "http://127.0.0.1:8000/currency_rates_diff";
+      axios({
+        method: "GET",
+        url: path,
+        params: {
+          code: code,
+          date1: date1,
+          date2: date2,
+        },
+      })
+        .then((res) => {
+          this.currencyRateDiff = res.data;
+        })
+        .catch((error) => {
+          this.errorMessage = error.response.data.detail;
+        });
+    },
+    setDefaultDates: function () {
+      const now = new Date();
+      const dd = now.getDate();
+      const mm = now.getMonth();
+      const yyyy = now.getFullYear();
+      let formateDate = [
+        (mm > 9 ? "" : "0") + mm,
+        (dd > 9 ? "" : "0") + dd,
+      ].join("-");
+      this.date1 = (yyyy - 1).toString() + "-" + formateDate;
+      this.date2 = yyyy.toString() + "-" + formateDate;
+    },
   },
+
   created() {
     this.getCurrencyCodes();
+    this.setDefaultDates();
+    this.getCurrencyRateDiff(this.selectedCode, this.date1, this.date2);
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 
 #alert {
-  color: red;
+  background-color: rgba(255, 90, 90, 0.5);
+  padding: 5px;
+  border-radius: 5px;
+  color: rgb(255, 53, 53);
+}
+
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: sans-serif;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+header {
+  display: none;
+}
+.box {
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.61);
+  margin: auto auto;
+  padding: 30px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: left;
+}
+.box h2 {
+  margin: 0 10px;
+  padding: 0;
+  color: #03a9f4;
+  text-align: center;
+}
+.box .input-date label {
+  color: #03a9f4;
+}
+.box .input-date input {
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid #03a9f4;
+  color: #03a9f4;
+  font-size: 18px;
+  letter-spacing: 1px;
+  margin-bottom: 30px;
+  outline: none;
+  padding: 5px;
+  width: 100%;
+  display: inline-block;
+}
+.box input[type="submit"],
+.box button[type="submit"],
+.box select,
+a.button {
+  font-family: sans-serif;
+  background: #03a9f4;
+  font-size: 12px;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: 600;
+  letter-spacing: 2px;
+  outline: none;
+  text-transform: uppercase;
+  text-decoration: none;
+  display: inline-block;
+  margin: 2px;
+  padding: 10px;
+}
+.box select {
+  float: right;
+  letter-spacing: 0;
+}
+
+.box input[type="submit"]:hover,
+.box button[type="submit"]:hover,
+.box select:hover,
+a.button:hover {
+  opacity: 0.8;
+}
+
+#diff {
+  font-size: 30px;
+  color: #03a9f4;
+  border: none;
+  text-decoration: underline #03a9f4;
+  text-align: center;
+  margin: 15px;
 }
 </style>
